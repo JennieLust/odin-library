649dd2e26efe8dfb513a8cdfac4b5b2c52d469b0
Jennie Lust <94746740+JennieLust@users.noreply.github.com>
2022-08-21 12:22:07 +0200

    genre submits properly through form

--- object.js
+++ object.js
@@ -1,12 +1,16 @@
 /*  TODO
 
-Add a NEW BOOK button that brings up a form allowing users to input the details for the new book: 
-    and anything else you might want.
+ISSUES: 
+    New object - genre is displayed before pages
 
-Add code that translates form values into javascript object
-    create object w it - Done, however only temporary objects are created
+Long-term goals: 
+    Add storage so new objects do not clear on reload
 
-Clear form when subm / closed 
+    Clear form when subm / closed 
+
+    Add accessibility
+
+    Organize code
 
  */
 
@@ -16,16 +20,18 @@ const addButton = document.querySelector("#new-book-btn");
 const form = document.querySelector(".form-popup");
 
 let myLibrary = []
-/* retrieves stored library */
+/* retrieves stored library 
 var storedLibrary = JSON.parse(localStorage.getItem("my_library"));
-myLibrary = storedLibrary;
+myLibrary = storedLibrary; */
 
 /* book constructor */
-function Book(title, author, pages, read) {
+function Book(title, author, pages, genre, read) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.genre = genre
     this.read = read
+    /* uneccesary, remove in future */
     this.info = function() {
         return title + " by " + author + ", " + pages + " pages, " + read
     };
@@ -60,11 +66,16 @@ function submitForm(form) {
     }
     
     /* dynamically names variables for new books */
-    eval['book'+myLibrary.length] = new Book(form.title.value, form.author.value, form.pages.value, read)
+    eval['book'+myLibrary.length] = new Book(form.title.value, form.author.value, form.genre.value, form.pages.value, read)
 
     /* adds object to library and reloads display */
     addBookToLibrary(eval['book'+myLibrary.length]);
     displayBooks();
+    form.title.value = "";
+    form.author.value = "";
+    form.genre.value = "";
+    form.pages.value = 1;
+    form.read.checked = false;
 };
 
 
@@ -75,13 +86,13 @@ function displayBooks() {
         var card = document.createElement("div");
         card.className = "card";
         bookContainer.appendChild(card);
-
+        /* creates card button container */
         const iconButtons = document.createElement("div");
         iconButtons.className = "icon-buttons";
-
+        /* card remove button and icon*/
         const btnRemove = document.createElement("button");
         btnRemove.className = "remove-button";
-        
+        /* Mouseover effect */
         const iconRemove = document.createElement("img");
         iconRemove.src = "./remove.png";
         iconRemove.className = "remove-icon"
@@ -90,15 +101,12 @@ function displayBooks() {
             iconRemove.addEventListener('mouseout', () => {
                 iconRemove.style.filter = "invert(100%) sepia(100%) saturate(2%) hue-rotate(88deg) brightness(109%) contrast(101%)"
             })
-        })
-
-       const btnRead = document.createElement("button");
+        });
+        /* card read/unread button and icon */
+        const btnRead = document.createElement("button");
         btnRead.className = "read-button";
         const iconUnread = document.createElement("img");
         iconUnread.className = "read-icon"
-         /* ./eyeopen.png */
-       
-
         
         /* iterates through the object and creates p items with property and value */
         Object.values(book).forEach(key => {
@@ -115,15 +123,20 @@ function displayBooks() {
                     text.className = "author";
                     text.innerText = `by ${key}`;
                     card.appendChild(text);
+                } else if (Object.keys(book).find(k => book[k] === key) === "genre"){
+                    var text = document.createElement("p");
+                    text.className = "genre";
+                    text.innerText = `${key}`;
+                    card.appendChild(text);
                 } else if (Object.keys(book).find(k => book[k] === key) === "pages"){
                     var text = document.createElement("p");
                     text.className = "pages";
-                    text.innerText = `${key} pages |  `;
+                    text.innerText = `${key} pages of `;
                     card.appendChild(text);
                 }  else {
                     var text = document.createElement("p");
                     text.className = "read";
-                    text.innerText = `${key}.`;
+                    text.innerText = `, which you have ${key}.`;
                     card.appendChild(text);
                     if (key === "not read"){
                         /* decides which read-icon to add */ 
@@ -157,14 +170,11 @@ function displayBooks() {
                 };
             };   
         });
-
         card.appendChild(iconButtons)
         iconButtons.appendChild(btnRemove)
         btnRemove.appendChild(iconRemove)
         btnRead.appendChild(iconUnread)
         iconButtons.appendChild(btnRead);
-    
-    
     });
 };
 
@@ -172,19 +182,23 @@ function displayBooks() {
 
 
 /* Adds default books to library */
-var theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read')
-var bookTwo = new Book('Book Two', 'Author Two des Authorieras', 300, 'read')
-var bookThree = new Book('Book Three', 'Ano Nomies', 15, 'not read')
-var bookFour = new Book('Book Four', 'Ano Nomies', 15, 'not read')
-var bookFive = new Book('Book Four & Nine Point Nine', 'Ano Nomies', 10005, 'not read')
+var theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'high fantasy', 'not read')
+var bookTwo = new Book('Book Two', 'Author Two des Authorieras', 300, 'murder mystery', 'read')
+var bookThree = new Book('Book Three', 'Ano Nomies', 15, 'greek tragedy', 'not read')
+var bookFour = new Book('Book Four', 'Ano Nomies', 15, 'post-apocalyptic comedy', 'not read')
+var bookFive = new Book('Book Four & Nine Point Nine', 'Ano Nomies', 10005, 'adventure action', 'not read')
 
-/* comment out/remove if stored array is retrieved otherwise it'll keep adding these to the library 
+/* comment out/remove if stored array is retrieved otherwise it'll keep adding these to the library */
 addBookToLibrary(theHobbit);
 addBookToLibrary(bookTwo);
 addBookToLibrary(bookThree);
 addBookToLibrary(bookFour);
 addBookToLibrary(bookFive);
-*/
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
+
 
 /* Calls display so it is loaded on page loag */
 displayBooks();
